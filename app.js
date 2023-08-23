7c36efd6cd224519924b9374f513c9dd84d1ad98
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-08-23 18:12:10 +0500

    basic callback explain

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-// Global execution context
-logger();
-console.log(1);
+// // Global execution context
+// logger();
+// console.log(1);
 
-// const logger = () => {
+// // const logger = () => {
+// //   console.log(2);
+// //   console.log(3);
+// // };
+
+// function logger() {
 //   console.log(2);
 //   console.log(3);
-// };
+// }
 
-function logger() {
-  console.log(2);
-  console.log(3);
+// function one() {
+//   console.log(1);
+// }
+
+// function two() {
+//   console.log(2);
+// }
+
+// function three() {
+//   console.log(3);
+// }
+
+// one();
+// two();
+// three();
+
+function message() {
+    console.log("hello")
 }
 
-function one() {
-  console.log(1);
+function logger(message) {
+    message()
 }
 
-function two() {
-  console.log(2);
-}
-
-function three() {
-  console.log(3);
-}
-
-one();
-two();
-three();
+logger(message)
