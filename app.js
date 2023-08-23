7d58199c06dd60d120f423a00532cf2ad2d29463
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-08-23 17:30:10 +0500

    basic console log to understand how js work

---
+++ app.js
@@ -0,0 +1,13 @@
+// Global execution context
+logger();
+console.log(1);
+
+// const logger = () => {
+//   console.log(2);
+//   console.log(3);
+// };
+
+function logger() {
+  console.log(2);
+  console.log(3);
+}
