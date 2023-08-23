9cd6e9d8dbe79b07072c98d8a7e3fa5e77651cae
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-08-23 18:34:44 +0500

    promise example 1

--- app.js
+++ app.js
@@ -1,53 +1,70 @@
-// // // Global execution context
-// // logger();
-// // console.log(1);
+// // // // Global execution context
+// // // logger();
+// // // console.log(1);
 
-// // // const logger = () => {
+// // // // const logger = () => {
+// // // //   console.log(2);
+// // // //   console.log(3);
+// // // // };
+
+// // // function logger() {
 // // //   console.log(2);
 // // //   console.log(3);
-// // // };
+// // // }
 
-// // function logger() {
-// //   console.log(2);
+// // function one() {
+// //   console.log(1);
+// // }
+
+// // function two() {
+// //   setTimeout(() => {
+// //     console.log(2);
+// //     three();
+// //   }, 0);
+// // }
+
+// // function three() {
 // //   console.log(3);
 // // }
 
-// function one() {
-//   console.log(1);
-// }
+// // one();
+// // two();
 
-// function two() {
+// // // function message() {
+// // //     console.log("hello")
+// // // }
+
+// // // function logger(message) {
+// // //     message()
+// // // }
+
+// // // logger(message)
+
+// function callbackHell() {
 //   setTimeout(() => {
-//     console.log(2);
-//     three();
-//   }, 0);
+//     const data = { user: 'Jams' };
+//     setTimeout(() => {
+//       console.log('Data has been Updated');
+//     }, 1000);
+//     console.log('we got some data');
+//   }, 500);
 // }
 
-// function three() {
-//   console.log(3);
-// }
+// callbackHell();
 
-// one();
-// two();
+const promise = new Promise((resolve, reject) => {
+  resolve('we did it baby');
+  //   reject('awe shit fucking server is down');
+});
 
-// // function message() {
-// //     console.log("hello")
-// // }
-
-// // function logger(message) {
-// //     message()
-// // }
-
-// // logger(message)
-
-function callbackHell() {
-  setTimeout(() => {
-    const data = { user: 'Jams' };
-    setTimeout(() => {
-      console.log('Data has been Updated');
-    }, 1000);
-    console.log('we got some data');
-  }, 500);
-}
-
-callbackHell();
+promise
+  .then((data) => {
+    const user = data;
+    return fetch('https://jsonplaceholder.typicode.com/todos/1');
+  })
+  .then((todos) => {
+    console.log(todos);
+  })
+  .catch((e) => {
+    console.error(e);
+  });
