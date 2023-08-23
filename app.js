02f49abdb850c3e2387f3cf214bdb611944aa849
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-08-23 18:17:46 +0500

    callback hell explain

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
-// // Global execution context
-// logger();
-// console.log(1);
+// // // Global execution context
+// // logger();
+// // console.log(1);
 
-// // const logger = () => {
+// // // const logger = () => {
+// // //   console.log(2);
+// // //   console.log(3);
+// // // };
+
+// // function logger() {
 // //   console.log(2);
 // //   console.log(3);
-// // };
+// // }
 
-// function logger() {
-//   console.log(2);
+// function one() {
+//   console.log(1);
+// }
+
+// function two() {
+//   setTimeout(() => {
+//     console.log(2);
+//     three();
+//   }, 0);
+// }
+
+// function three() {
 //   console.log(3);
 // }
 
-function one() {
-  console.log(1);
-}
+// one();
+// two();
 
-function two() {
+// // function message() {
+// //     console.log("hello")
+// // }
+
+// // function logger(message) {
+// //     message()
+// // }
+
+// // logger(message)
+
+function callbackHell() {
   setTimeout(() => {
-    console.log(2);
-    three();
-  }, 0);
+    const data = { user: 'Jams' };
+    setTimeout(() => {
+      console.log('Data has been Updated');
+    }, 1000);
+    console.log('we got some data');
+  }, 500);
 }
 
-function three() {
-  console.log(3);
-}
-
-one();
-two();
-
-// function message() {
-//     console.log("hello")
-// }
-
-// function logger(message) {
-//     message()
-// }
-
-// logger(message)
+callbackHell();
