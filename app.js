9306bfa70fca2e1da39398476c519d493ae83dbc
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-08-23 17:55:33 +0500

    call stack explain in js

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ function logger() {
   console.log(2);
   console.log(3);
 }
+
+function one() {
+  console.log(1);
+}
+
+function two() {
+  console.log(2);
+}
+
+function three() {
+  console.log(3);
+}
+
+one();
+two();
+three();
