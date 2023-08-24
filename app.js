1278a3ad216d1eb9238f7d061eb94eb8dea459e3
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-08-24 12:14:46 +0500

    explain fetch as promise

--- app.js
+++ app.js
@@ -1,78 +1,84 @@
-// // // // // Global execution context
-// // // // logger();
-// // // // console.log(1);
+// // // // // // Global execution context
+// // // // // logger();
+// // // // // console.log(1);
 
-// // // // // const logger = () => {
+// // // // // // const logger = () => {
+// // // // // //   console.log(2);
+// // // // // //   console.log(3);
+// // // // // // };
+
+// // // // // function logger() {
 // // // // //   console.log(2);
 // // // // //   console.log(3);
-// // // // // };
+// // // // // }
 
-// // // // function logger() {
-// // // //   console.log(2);
+// // // // function one() {
+// // // //   console.log(1);
+// // // // }
+
+// // // // function two() {
+// // // //   setTimeout(() => {
+// // // //     console.log(2);
+// // // //     three();
+// // // //   }, 0);
+// // // // }
+
+// // // // function three() {
 // // // //   console.log(3);
 // // // // }
 
-// // // function one() {
-// // //   console.log(1);
-// // // }
+// // // // one();
+// // // // two();
 
-// // // function two() {
+// // // // // function message() {
+// // // // //     console.log("hello")
+// // // // // }
+
+// // // // // function logger(message) {
+// // // // //     message()
+// // // // // }
+
+// // // // // logger(message)
+
+// // // function callbackHell() {
 // // //   setTimeout(() => {
-// // //     console.log(2);
-// // //     three();
-// // //   }, 0);
+// // //     const data = { user: 'Jams' };
+// // //     setTimeout(() => {
+// // //       console.log('Data has been Updated');
+// // //     }, 1000);
+// // //     console.log('we got some data');
+// // //   }, 500);
 // // // }
 
-// // // function three() {
-// // //   console.log(3);
-// // // }
+// // // callbackHell();
 
-// // // one();
-// // // two();
+// // const promise = new Promise((resolve, reject) => {
+// //   resolve('we did it baby');
+// //   //   reject('awe shit fucking server is down');
+// // });
 
-// // // // function message() {
-// // // //     console.log("hello")
-// // // // }
-
-// // // // function logger(message) {
-// // // //     message()
-// // // // }
-
-// // // // logger(message)
-
-// // function callbackHell() {
-// //   setTimeout(() => {
-// //     const data = { user: 'Jams' };
-// //     setTimeout(() => {
-// //       console.log('Data has been Updated');
-// //     }, 1000);
-// //     console.log('we got some data');
-// //   }, 500);
-// // }
-
-// // callbackHell();
+// // promise
+// //   .then((data) => {
+// //     const user = data;
+// //     return fetch('https://jsonplaceholder.typicode.com/todos/1');
+// //   })
+// //   .then((todos) => {
+// //     console.log(todos);
+// //   })
+// //   .catch((e) => {
+// //     console.error(e);
+// //   });
 
 // const promise = new Promise((resolve, reject) => {
-//   resolve('we did it baby');
-//   //   reject('awe shit fucking server is down');
+//   setTimeout(() => resolve('this is my data from a server "by God!"'), 2000);
 // });
 
-// promise
-//   .then((data) => {
-//     const user = data;
-//     return fetch('https://jsonplaceholder.typicode.com/todos/1');
-//   })
-//   .then((todos) => {
-//     console.log(todos);
-//   })
-//   .catch((e) => {
-//     console.error(e);
-//   });
+// promise.then((res) => {
+//   console.log(res);
+// });
 
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => resolve('this is my data from a server "by God!"'), 2000);
-});
-
-promise.then((res) => {
-  console.log(res);
-});
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then((res) => res.json())
+//   .then((res) => console.log(res))
+//   .catch((e) => console.log(e))
+//   .finally(e => console.log('F*** you function complete'));
